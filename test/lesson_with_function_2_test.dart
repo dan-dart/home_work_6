fab9654e0aaeddd85781e84875064a95d9141b70
Eshmatov Daniir <eshdan0@gmail.com>
2024-02-15 15:39:24 +0600

    AAA

---
+++ test/lesson_with_function_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:lesson_with_function_2/lesson_with_function_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
